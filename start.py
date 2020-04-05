a5b4b86dafe85506e9088a0f2bceff0ffba482d1
罗印 <912271673@qq.com>
2020-04-05 17:19:34 +0800

    update

--- start.py
+++ start.py
@@ -1,11 +1,5 @@
 import time
-print("Starting",end="")
-time.sleep(0.5)
-print(".",end="")
-time.sleep(0.5)
-print(".",end="")
-time.sleep(0.5)
-print(".")
+print("Starting...")
 shell()
 def shell():
     print("Esp32 shell 0.1 for Esp32 Os 0.1")
