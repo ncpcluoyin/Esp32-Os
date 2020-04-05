0adbfba1188be5cf41a42907bdb667a3bfae2305
罗印 <912271673@qq.com>
2020-04-05 17:08:36 +0800

    update start.py

--- start.py
+++ start.py
@@ -1,1 +1,17 @@
-print("hello world")
+print("Starting Esp32 Os...")
+print("Esp32 Os 0.1")
+print("running init.py")
+try:
+    file = open("init.py")
+    exec(file.read())
+    print("OK")
+except:
+    print("error!file not found!")
+    return
+print("starting shell...")
+try:
+    file = open("shell.py")
+    exec(file.read())
+except:
+    print("error!file not found!")
+    return
