87e22c77df9fb2d0288871db3aa9107d1154854a
罗印 <912271673@qq.com>
2020-04-05 16:51:24 +0800

    update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ print("Esp32 Os Booter")
 print("Boot from \"start.py\"")
 try:
     file = open("start.py")
+    exec(file.read())
 except:
     print("system not found")
-exec(file.read())
 file.close()
 print("system halt")
